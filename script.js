858d80fd08347d685f978122f3770748e41095fe
ousama lasri <ousamalasri2@gmail.com>
2022-10-30 15:44:51 +0100

    test

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+    
+let NomProduit = document.getElementById("nomProduit");
+let PrixProduit = document.getElementById("prixProduit");
+
+
+
+let NomP1 = document.getElementById("NomP1").innerText;
+let PrixP1 = document.getElementById("PrixP1").innerText;
+console.log(NomP1);
+
+
+
+NomProduit.innerHTML =  NomP1; 
+PrixProduit.innerHTML = PrixP1 ;
