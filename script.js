599786492a73b239cd34502e864aeb5683125459
ousama lasri <ousamalasri2@gmail.com>
2022-10-31 18:58:38 +0100

    3eme livrable

--- script.js
+++ script.js
@@ -1,16 +1,67 @@
+let makiClass = document.querySelector(".Maki");
+let nigiriClass = document.querySelector(".Nigiri");
+let soupeClass = document.querySelector(".Soupe");
+let btnTous = document.getElementById("IdTous");
+let btnMaki = document.getElementById("IdMaki");
+let btnSoupe = document.getElementById("IdSoupe");
+let btnNigiri = document.getElementById("IdNigiri");
+let arrTotal = [];
+var somme = 0 ;
 
 
-    
-let NomProduit = document.getElementById("nomProduit");
-let PrixProduit = document.getElementById("prixProduit");
+btnTous.addEventListener('click',function(){
+    makiClass.style.display="flex";
+    nigiriClass.style.display="flex";
+    soupeClass.style.display="flex";
+
+});
+btnMaki.addEventListener('click',function(){
+    makiClass.style.display="flex";
+    nigiriClass.style.display="none";
+    soupeClass.style.display="none";
+
+});
+btnSoupe.addEventListener('click',function(){
+    makiClass.style.display="none";
+    nigiriClass.style.display="none";
+    soupeClass.style.display="flex";
+
+});
+btnNigiri.addEventListener('click',function(){
+    makiClass.style.display="none";
+    nigiriClass.style.display="flex";
+    soupeClass.style.display="none";
+
+});
+
+
+let eventclickAdd = document.querySelectorAll('.btn-add');
+let parentPanier = document.querySelector('.Produit');
+var ArrLenght = 0;
 
 
 
-let NomP1 = document.getElementById("NomP1").innerText;
-let PrixP1 = document.getElementById("PrixP1").innerText;
-console.log(NomP1);
+eventclickAdd.forEach(function(element){
+    element.addEventListener('click',function(e){
+        let obj = e.target.parentNode.parentNode;
+        let titreProduit = obj.childNodes[3].childNodes[1].innerText;
+        let prixProduit = obj.childNodes[7].childNodes[1].innerText.replace("$", "");
+        let total = document.getElementById('TotalAchat');
+        // split("$")[0]
+        let div = document.createElement("div");
+        div.innerHTML=`<div class="Produit-acheter">
+        <div class="nom-produit">${titreProduit}</div>
+        <div class="prix-produit">${prixProduit}</div>
+        </div>` ;
+        parentPanier.appendChild(div);  
+        // parentPanier.insertBefore(div,document.querySelector('.Class-total'));
+        // console.log(document.querySelector('.Class-total').parentNode);
+        arrTotal.push(parseInt(prixProduit));
+        somme = arrTotal.reduce((p, c)=>p+c, 0) ;
+        // console.log(somme);
+        total.innerHTML=somme;       
+    });
+});
 
 
 
-NomProduit.innerHTML =  NomP1; 
-PrixProduit.innerHTML = PrixP1 ;
